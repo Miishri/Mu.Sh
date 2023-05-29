c449ab4fde35535690e4fa01de8d687493673606
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-05-30 01:31:57 +0200

    add project class & refactor main.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,43 @@
 import './style.css'
+import { Project } from './project'
 
-function createProject() {
-    let project = document.createElement('div') as HTMLDivElement;
+
+const arrayValue = [""]; // array for project objects
+
+const projects: HTMLDivElement = <HTMLDivElement>document.querySelector('.projects');
+
+function createProject(projectObject: Project) {
+    const project: HTMLDivElement = <HTMLDivElement>document.createElement('div');
     project.classList.add('project');
-    
+    projects.append(project);
+
+    const projectTitle: HTMLDivElement = <HTMLDivElement>document.createElement('div');
+    projectTitle.classList.add('title');
+    projectTitle.textContent = projectObject.title;
+    project.append(projectTitle);
+
+    const pic_card: HTMLDivElement = <HTMLDivElement>document.createElement('div');
+    pic_card.classList.add('pic-card');
+    project.append(pic_card);
+
+    const pic_inner: HTMLDivElement = <HTMLDivElement>document.createElement('div');
+    pic_inner.classList.add('pic-inner');
+    pic_card.append(pic_inner);
+
+    const pic_front: HTMLDivElement = <HTMLDivElement>document.createElement('div');
+    pic_front.classList.add('pic-front');
+    pic_inner.append(pic_front);
+
+    const pic_image: HTMLDivElement = <HTMLDivElement>document.createElement('img') as HTMLImageElement;
+    pic_image.classList.add('image');
+    pic_front.append(pic_image);
+
+    const pic_back = document.createElement('div') as HTMLDivElement;
+    pic_back.classList.add('pic-back');
+    pic_inner.append(pic_back);
+
+    const pic_description = document.createElement('div') as HTMLDivElement;
+    pic_description.classList.add('desc');
+    pic_description.textContent = projectObject.description;
+    pic_back.append(pic_description);
 }
---
+++ src/ts/project.ts
@@ -0,0 +1,15 @@
+import './style.css'
+
+export class Project {
+    public title: string;
+    public description: string;
+    public image: string;
+    public link: string;
+
+    constructor(title: string, description: string, image: string, link: string) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.link = link;
+    }
+}
