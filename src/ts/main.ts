58b0bd660bfad3c9a9877ef26efcb638d90e5a18
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-05-30 16:56:58 +0200

    test edit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -42,4 +42,24 @@ function createProject(projectObject: Project) {
     pic_back.append(pic_description);
 }
 
+let arrayValue: Project[] = []; // array for project objects
+
+const projectTest: Project = new Project
+(
+    "Test_1", 
+    "Hello i am test 1 ", 
+    "https://picsum.photos/200/300", 
+    "https://www.google.com/"
+);
+
+const projectTest2: Project = new Project
+(
+    "Test_2", 
+    "Hey i am test 2", 
+    "https://picsum.photos/200/300", 
+    "https://www.google.com/"
+);
+
+arrayValue.push(projectTest, projectTest2);
+
 arrayValue.forEach(each => createProject(each))
